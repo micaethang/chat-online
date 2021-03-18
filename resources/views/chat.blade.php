ec0bd4d9733a0f8c4e65c87bfdb3a4233af9b7bd
Thang Nguyen Minh <thang.nm@neo-lab.vn>
2021-03-18 20:25:37 +0700

    Design simple scroll

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -5,17 +5,25 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  <style>
+    .list-group {
+      overflow-y: scroll;
+      height: 200px;
+    }
+  </style>
 </head>
 <body>
   <div class="container">
     <div class="row" id="app">
-      <ul class="list-group offset-4 col-4">
+      <div class="offset-4 col-4">
         <li class="list-group-item active">Chat Room</li>
-        <message v-for="value in chat.message">
-          @{{ value }}
-        </message>
+        <ul class="list-group">
+          <message v-for="value in chat.message">
+            @{{ value }}
+          </message>
+        </ul>
         <input type="text" class="form-control" placeholder="Type your message here..." v-model='message' v-on:keyup.enter='send'>
-      </ul>
+      </div>
     </div>
   </div>
 
