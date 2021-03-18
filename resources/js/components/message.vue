8da85dec532dcad9d109be3344351ccc69336aff
Thang Nguyen Minh <thang.nm@neo-lab.vn>
2021-03-18 20:00:02 +0700

    Create component message

---
+++ resources/js/components/message.vue
@@ -0,0 +1,11 @@
+<template>
+    <li class="list-group-item">A second item</li>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -11,10 +11,7 @@
     <div class="row" id="app">
       <ul class="list-group offset-4 col-4">
         <li class="list-group-item active">Chat Room</li>
-        <li class="list-group-item">A second item</li>
-        <li class="list-group-item">A third item</li>
-        <li class="list-group-item">A fourth item</li>
-        <li class="list-group-item">And a fifth one</li>
+        <message></message>
         <input type="text" class="form-control" placeholder="Type your message here..." v-model='message' v-on:keyup.enter='send'>
       </ul>
     </div>
