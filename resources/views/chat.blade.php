6c146c141a747b31cb17aea5cd21cf1886468f86
Thang Nguyen Minh <thang.nm@neo-lab.vn>
2021-03-18 19:30:15 +0700

    Add route chat

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+    <div class="container">
+        <div class="row" id="app">
+            <h1>Chat Room</h1>
+        </div>
+    </div>
+
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
